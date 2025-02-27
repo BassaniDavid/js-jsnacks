3828d9fcd0adf1abc1cfe9a48fd388adff4988a9
BassaniDavid <davbassan98@gmail.com>
2025-02-27 12:14:54 +0100

    feat: snack1

---
+++ jsnack1/app.js
@@ -0,0 +1,13 @@
+// jsnack1
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore
+let firstNumber;
+let secondNumber;
+firstNumber = prompt('inserisci il primo numero')
+secondNumber = prompt('inserisci il secondo numero')
+
+if(firstNumber>secondNumber){
+    console.log(firstNumber)
+} else{
+    console.log(secondNumber)
+}
